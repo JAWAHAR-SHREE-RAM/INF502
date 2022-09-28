e9d76a1ff8046e906f148dfe5bbb70bae6926d9d
Jawahar Shree Ram Ravi <jawaharshreeramr@gmail.com>
2022-09-28 09:14:30 -0700

    Update Assingment2_p1.py

--- Assingment2_p1.py
+++ Assingment2_p1.py
@@ -1,14 +1,12 @@
-from math import hypot
-#import math
+from math import hypot  #importing hypot fucntion from math library
 
-
-def pythagoreanTheorem(a, b):
+def pythagoreanTheorem(a, b): #pythagorean theorem function used to calcualte hypotenues
 	c = hypot(a, b)
 	print(c)
 
 
-a = int(input("Enter the length a : "))
-b = int(input("Enter the length b : "))
-pythagoreanTheorem(a, b)
+a = int(input("Enter the length a : "))  #Getting lenght of side A from user
+b = int(input("Enter the length b : "))  #Getting lenght of side B from user
+pythagoreanTheorem(a, b)				 #Calling the function to perform the calculation
 
 
